a2df7c6f89986d96f980109cf891d6c2b1ac1fba
Sukriti Sarkar <tosukriti5@gmail.com>
2018-08-17 22:17:04 +0530

    Creating LetsExploreJava class

---
+++ LetsExploreJava.java
@@ -0,0 +1,15 @@
+package ss.java;
+
+public class LetsExploreJava {
+
+	
+	public static void main(String[] args) {
+		System.out.println(System.getenv());
+		System.out.println("This is your system's Line separator:" + System.lineSeparator() + "Okay!!");
+		
+		System.out.println(System.out.append("Appending"));
+		System.out.println(System.out.getClass());
+		
+	}
+
+}
